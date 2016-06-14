b90e772ce4536f6694d4425470748271761c4909
Claudiu <claudiuch113@gmail.com>
2016-06-14 14:58:42 +0300

    First JUnit3 Test file

---
+++ src/test/com/leon/CVNator/web/MainControllerTest.java
@@ -0,0 +1,45 @@
+package com.leon.CVNator.web;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ClaudiuDiana on 6/14/2016.
+ */
+public class MainControllerTest extends TestCase {
+    public void testRegistration() throws Exception {
+
+    }
+
+    public void testRegistration1() throws Exception {
+
+    }
+
+    public void testGenerateCV() throws Exception {
+
+    }
+
+    public void testGenerateCV1() throws Exception {
+
+    }
+
+    public void testAddNewField() throws Exception {
+
+    }
+
+    public void testGenerateCV2() throws Exception {
+
+    }
+
+    public void testLogin() throws Exception {
+
+    }
+
+    public void testWelcome() throws Exception {
+
+    }
+
+    public void testInitBinder() throws Exception {
+
+    }
+
+}
