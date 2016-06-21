99d465f4a91c7261900462907e36ff0b7ddbc370
Vibesx <lucp24@yahoo.com>
2016-06-21 09:24:00 +0300

    adding service test folder

---
+++ src/test/com/leon/CVNator/service/UserServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.leon.CVNator.service;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Claudiu on 6/20/2016.
+ */
+public class UserServiceImplTest extends TestCase {
+    public void testSave() throws Exception {
+
+    }
+
+    public void testFindByUsername() throws Exception {
+
+    }
+
+}
